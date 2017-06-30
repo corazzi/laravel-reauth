8795be40041ae22a603505d1adb72fd45347ff46
Sacha Corazzi <sacha.corazzi@gmail.com>
2017-06-30 13:22:28 +0000

    Clean up commits and middleware handle() method

--- src/ReauthMiddleware.php
+++ src/ReauthMiddleware.php
@@ -17,20 +17,26 @@ class ReauthMiddleware
      */
     public function handle($request, Closure $next)
     {
-        if (! Auth::check()) {
-            return $this->fail();
-        }
-
-        $reauthAttempt = Auth::validate([
-            'email'    => $request->user()->email,
-            'password' => $request->get('reauth_password'),
-        ]);
-
-        if (! $reauthAttempt) {
+        if (! Auth::check() || ! $this->attempt($request)) {
             return $this->fail();
         }
 
         return $next($request);
+    }
+
+    /**
+     * Attempt to reauthenticate the user
+     *
+     * @param  \Illuminate\Http\Request $request
+     *
+     * @return bool
+     */
+    protected function attempt($request)
+    {
+        return Auth::validate([
+            'email'    => $request->user()->email,
+            'password' => $request->get('reauth_password'),
+        ]);
     }
 
     /**
@@ -40,36 +46,31 @@ class ReauthMiddleware
      */
     public function fail()
     {
-        /**
-         * Maybe log the user out
-         */
+        // Maybe log the user out
         if (config('reauth.logout')) {
             Auth::logout();
         }
 
-        /**
-         * Throw an exception
-         */
+        // Throw an exception
         if (($onFail = config('reauth.onFail')) == 'exception') {
+
+            // Determine which exception to use – either a custom one or the default ReauthFailedException
             $exception = config('reauth.exception.class', ReauthFailedException::class);
 
+            // Throw the exception, passing a customisable exception message
             throw new $exception(
                 config('reauth.exception.message', 'Reauth failed')
             );
         }
 
-        /**
-         * Redirect back with an error message
-         */
+        // Redirect back with an error message
         if ($onFail == 'redirect') {
             return redirect()->back()->withErrors([
-                config('reauth.redirect.message')
+                config('reauth.redirect.message'),
             ]);
         }
 
-        /**
-         * If we reach this point, something has been misconfigured
-         */
+        // If we reach this point, something has been misconfigured
         throw new ReauthFailedException(
             'Password could not be verified. Also, there is a reauth misconfiguration'
         );
